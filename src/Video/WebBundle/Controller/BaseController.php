4ed1455f16ef1497a3304bd29ea2fa02e75dd704
woo <feiyu06@woodeMac-mini.local>
2014-11-17 22:44:58 +0800

    Json Enhancemanted

--- src/Video/WebBundle/Controller/BaseController.php
+++ src/Video/WebBundle/Controller/BaseController.php
@@ -5,9 +5,20 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class BaseController extends Controller
 {
-	protected function createJsonResponse($data)
-	{
-	        	return new JsonResponse($data);
+	/**现在JSON信息回调需要封装三个参数：
+	* data:回调数据
+	* status:回调状态，标示数据状态正确与否，1为成功，0为败
+	* message:回调消息，反映错误原因
+	*/
+	protected function createJsonResponse($data,$status,$message)
+	{			
+				$response = array(
+					'data'=>$data,
+					'status'=>$status,
+					'message'=>$message
+				);
+	        	return new JsonResponse($response);
 	}
+
 
 }
